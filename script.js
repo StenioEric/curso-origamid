b4173987318e38f1270c67d2d69b44fc5a69e406
StenioEric <stenioeric@gmail.com>
2024-10-19 16:28:17 -0300

    Verificações de erro de js e bronwser não suportar

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-const tabMenu = document.querySelectorAll('.js-tabmenu li');
-const tabContent = document.querySelectorAll('.js-tabcontent section');
-tabContent[0].classList.add('ativo');
+const tabMenu = document.querySelectorAll(".js-tabmenu li");
+const tabContent = document.querySelectorAll(".js-tabcontent section");
+tabContent[0].classList.add("ativo");
 
-function activeTab(index) {
+if (tabMenu.length && tabContent.length) {
+  function activeTab(index) {
     tabContent.forEach((section) => {
-        section.classList.remove('ativo');
+      section.classList.remove("ativo");
     });
 
-    tabContent[index].classList.add('ativo');
+    tabContent[index].classList.add("ativo");
+  }
+
+  tabMenu.forEach((itemMenu, index) => {
+    itemMenu.addEventListener("click", () => {
+      activeTab(index);
+    });
+  });
 }
-
-
-tabMenu.forEach((itemMenu, index) =>{
-    itemMenu.addEventListener('click', () => {
-        activeTab(index);
-    });
-});
-
