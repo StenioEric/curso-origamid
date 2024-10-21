44ba444f0dea4297649a97ad75e336be22a8c8a1
StenioEric <stenioeric@gmail.com>
2024-10-21 10:46:35 -0300

    Implementada função de scroll suave na página

--- script.js
+++ script.js
@@ -39,3 +39,31 @@ function initAccordion() {
   }
 }
 initAccordion();
+
+function initScrollSuave (){
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  function scrollSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+    
+    // const topo = section.offsetTop;
+    // window.scrollTo({
+    //   top: topo,
+    //   behavior: 'smooth',
+    // });
+
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  }
+
+  // Iteração para adicionar event listener a cada link
+  linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollSection);
+  });
+}
+
+initScrollSuave();
