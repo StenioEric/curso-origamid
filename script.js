3754d3a29a43bf5cd7dfbd8e828b88639e589a0b
StenioEric <stenioeric@gmail.com>
2024-10-22 22:04:46 -0300

    Adicionada função de animação no scroll

--- script.js
+++ script.js
@@ -67,3 +67,21 @@ function initScrollSuave (){
 }
 
 initScrollSuave();
+
+function initAnimaScroll() {
+  const sections = document.querySelectorAll('.js-scroll');
+  if (section.length) {
+    const windowMetade = window.innerHeight * 0.6;
+
+    function animaScroll() {
+      sections,forEach((section) => {
+        const sectionTop = section.getBoundingCleintReact().top;
+        const isSectionVisible = (section - windowMetade) < 0;
+        if(isSectionVisible)
+          section.classList.add('ativo')
+      })
+    }
+    animaScroll();
+    window.addEventListener('scroll', animaScroll)
+  }
+}
